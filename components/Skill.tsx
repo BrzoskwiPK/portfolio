dbbf3e3263f75ee42e72890c06fda84279fb7322
Gabriel Brzoskwinia <brzoskwinia.gabriel@gmail.com>
2024-01-14 22:08:16 +0100

    fixes

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -17,7 +17,7 @@ const Skill = ({ skill, directionLeft }: Props) => {
         }}
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, x: 0 }}
-        src={urlFor(skill?.image).url()}
+        src={urlFor(skill?.image)?.url()}
         className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-32 md:h-32 xl:w-36 xl:h-36 filter group-hover:grayscale transition duration-300 ease-in-out'
       />
     </div>
